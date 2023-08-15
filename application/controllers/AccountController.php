86ef687267136faff0b2b60bedb64f825545feb1
LedSquare <prophet3334@gmail.com>
2023-08-15 21:54:06 +0300

    dev: added return types in methods

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -7,12 +7,12 @@ use application\core\Controller;
 class AccountController extends Controller
 {
     
-    public function loginAction()
+    public function loginAction(): void
     {
         echo 'Login page';
     }
 
-    public function registerAction()
+    public function registerAction(): void
     {
         echo 'Register page<br>';
         // var_dump($this->route);
