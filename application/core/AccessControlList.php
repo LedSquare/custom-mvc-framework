2326c96f7e33533f8b2412ce517fb86b039c47a6
LedSquare <prophet3334@gmail.com>
2023-08-23 11:20:05 +0300

    dev: Added ACL class

---
+++ application/core/AccessControlList.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace application\core;
+
+class AccessControlList 
+{
+    public array $config;
+    public array $route;
+
+    public function __construct($route)
+    {   
+        $this->route = $route;
+        $this->config = require 'application/acl/' . $this->route['controller'] . '.php';
+    }
+
+    public function checkAcl(): bool
+    {
+
+        if ($this->isAcl('all')) {
+            return true;
+        } elseif (isset($_SESSION['authorize']['id']) && $this->isAcl('authorize')) {
+            return true;
+        }
+        return false;
+    }
+
+    protected function isAcl($key): bool
+    {
+        return in_array($this->route['action'], $this->config[$key]);
+    }
+}
